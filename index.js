2f0c19c99b697adfb4e670b4c067491f5cf9fb72
Edith Gatwiri <edithgatwiri70@gmail.com>
2024-11-28 21:42:20 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+// Distance from HQ in blocks
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42;  
+    return Math.abs(pickupLocation - hqLocation);  
+  }
+  
+  console.log(distanceFromHqInBlocks(50));  
+  
+  // Distance from HQ in feet
+  function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);  
+    return blocks * 264;  
+  }
+  
+  console.log(distanceFromHqInFeet(34));  
+  
+  // Distance travelled in feet
+  function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(destination - start); 
+    return blocksTravelled * 264;  
+  }
+  
+  console.log(distanceTravelledInFeet(34, 38)); 
+  
+  // Fare price calculation
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+  
+    if (distanceInFeet <= 400) {
+      return 0;  
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      return (distanceInFeet - 400) * 0.02;  
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+      return 25;  
+    } else {
+      return 'cannot travel that far';  
+    }
+  }
+  
+  console.log(calculatesFarePrice(34, 38));  
